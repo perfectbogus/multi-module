816ebed066672f5647589e3cc1df91ef45caa2e9
perfectbogus <war_army_10@hotmail.com>
2026-09-21 20:40:08 -0600

    MongoDB: Start Task Enpoint

---
+++ mongo-svc/src/main/java/dev/perfectbogus/mongo/svc/MongoSvcApplication.java
@@ -0,0 +1,13 @@
+package dev.perfectbogus.mongo.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoSvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoSvcApplication.class, args);
+    }
+
+}
---
+++ mongo-svc/src/test/java/dev/perfectbogus/mongo/svc/MongoSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.perfectbogus.mongo.svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoSvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
